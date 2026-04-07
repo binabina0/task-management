fa1ce38eebe0a16f065b8cbf906841c3d1405969
binabina0 <malbinca.1111@gmail.com>
2026-04-07 12:19:34 +0600

    fix userService

--- src/main/java/com/example/demo/user/UserController.java
+++ src/main/java/com/example/demo/user/UserController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
+
+
     private final UserService userService;
 
     @PostMapping
