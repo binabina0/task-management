60369dff758d564c66d1085bc38a5d5bf4337ce6
binabina0 <malbinca.1111@gmail.com>
2026-04-15 16:50:22 +0600

    add task dto, add getTaskByUser

--- src/main/java/com/example/demo/task/TaskRepository.java
+++ src/main/java/com/example/demo/task/TaskRepository.java
@@ -1,8 +1,11 @@
 package com.example.demo.task;
 
+import com.example.demo.task.dto.TaskResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TaskRepository extends JpaRepository<Task, UUID> {
+    List<Task> findByAssignedToId(UUID userId);
 }
--- src/main/java/com/example/demo/task/TaskService.java
+++ src/main/java/com/example/demo/task/TaskService.java
@@ -2,11 +2,14 @@ package com.example.demo.task;
 
 import com.example.demo.group.Group;
 import com.example.demo.group.GroupRepository;
+import com.example.demo.task.dto.TaskRequest;
+import com.example.demo.task.dto.TaskResponse;
 import com.example.demo.user.UserEntity;
 import com.example.demo.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -16,23 +19,37 @@ public class TaskService {
     private final GroupRepository groupRepository;
     private final TaskRepository taskRepository;
 
-    public Task createTask(String title,
-                           String description,
-                           UUID groupId,
-                           UUID assignedToId,
-                           UUID createdById) {
-        UserEntity user = userRepository.findById(assignedToId).orElseThrow();
-        UserEntity creator = userRepository.findById(createdById).orElseThrow();
-        Group group = groupRepository.findById(groupId).orElseThrow();
+    public TaskResponse createTask(TaskRequest request) {
+        UserEntity user = userRepository.findById(request.getAssignedToId()).orElseThrow();
+        UserEntity creator = userRepository.findById(request.getCreatedById()).orElseThrow();
+        Group group = groupRepository.findById(request.getGroupId()).orElseThrow();
         Task task = Task.builder()
-                .title(title)
-                .description(description)
+                .title(request.getTitle())
+                .description(request.getDescription())
                 .status(TaskStatus.TODO)
                 .group(group)
                 .assignedTo(user)
                 .createdBy(creator)
                 .build();
-        return taskRepository.save(task);
+        Task saved =  taskRepository.save(task);
+        return TaskResponse.builder()
+                .id(saved.getId())
+                .title(saved.getTitle())
+                .status(saved.getStatus().name())
+                .assignedToName(user.getName())
+                .build();
 
+    }
+
+    public List<TaskResponse> getTaskByUser(UUID userId) {
+        return taskRepository.findByAssignedToId(userId)
+                .stream()
+                .map(task -> TaskResponse.builder()
+                        .id(task.getId())
+                        .title(task.getTitle())
+                        .status(task.getStatus().name())
+                        .assignedToName(task.getAssignedTo().getName())
+                        .build())
+                .toList();
     }
 }
